e90c7cf1151654fa595663926e6702433d727aea
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-17 09:55:39 +0600

    dom recap 17-4-26

--- recap.js
+++ recap.js
@@ -7,4 +7,19 @@
     2-3: querySelector
     2-4: quetySelectorAll
 
+ 3. dynamically add element to the dom : document.createElement 
+     3-5" set value to the innetHTML
+     
+ 4. get and set value from/to a dom element 
+    4-1: use innerHTML (h1, p, h5, div, span, strong)
+    4-2: use value (input, textArea)
+    
+ 5. add event lister (event hendler)
+      5-1: click, focus, blur, onchange, keyup
+      
+
+6. use function to reduce duplicate code
+
+
+
 */
