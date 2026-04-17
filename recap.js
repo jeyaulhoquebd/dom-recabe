4b516635f4b63da6629dc611f9720d81798d9863
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-17 09:46:34 +0600

    dom recap 17-4-26

---
+++ recap.js
@@ -0,0 +1,10 @@
+/* 
+
+1. How to ude js with html and css
+2. get element or elements from dom
+    2-1: getElementById
+    2-2: getElementByClassName
+    2-3: querySelector
+    2-4: quetySelectorAll
+
+*/
