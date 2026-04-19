6ca3dc9c225378d50c43a981cb9e891c1e08bf47
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-19 21:44:32 +0600

    git push

--- vaildation.js
+++ vaildation.js
@@ -1,8 +1,16 @@
 const number = 23;
+// string, number , boolean
 if(typeof number === 'number'){
+    console.log('value is a number')
 
 }
 
 else{
     console.log('value is not a number')
 }
+
+const numbers = [3, 12, 78, 90];
+console.log(typeof numbers);
+
+const student = {name : 'Rongila Rafsan', job: 'khawadawa kora'}
+console.log(typeof student);
